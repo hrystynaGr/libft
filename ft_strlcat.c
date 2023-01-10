28b4d8900b350658a1e11095b5438e438b8a4830
anna-balan <dp101292baa@gmail.com>
2023-01-10 20:56:23 +0100

    added strlcat function

---
+++ ft_strlcat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbalan <hbalan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/10 20:54:20 by hbalan            #+#    #+#             */
+/*   Updated: 2023/01/10 20:54:23 by hbalan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	int				i;
+	unsigned int	dest_size;
+
+	i = 0;
+	dest_size = 0;
+	while (dest[dest_size])
+		dest_size++;
+	while (src[i] && (dest_size < (size - 1)))
+	{
+		dest[dest_size] = src[i];
+		dest_size++;
+		i++;
+	}
+	dest[dest_size] = '\0';
+	return (dest_size);
+}

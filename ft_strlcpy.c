8030885b59a836cd20d13fc9abf441375cfb8afd
anna-balan <dp101292baa@gmail.com>
2023-01-12 15:50:03 +0100

    changes in strlcat, added strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,30 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbalan <hbalan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/10 20:54:20 by hbalan            #+#    #+#             */
-/*   Updated: 2023/01/10 20:54:23 by hbalan           ###   ########.fr       */
+/*   Created: 2023/01/12 15:07:59 by hbalan            #+#    #+#             */
+/*   Updated: 2023/01/12 15:08:03 by hbalan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	size_t				i;
-	size_t				dest_size;
+	size_t	i;
 
 	i = 0;
-	dest_size = 0;
-	while (dest[dest_size])
-		dest_size++;
-	while (src[i] && (dest_size < (size - 1)))
+	if (size != 0)
 	{
-		dest[dest_size] = src[i];
-		dest_size++;
-		i++;
+	size -= 1;
+		while (src[i] && (i < size))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[dest_size] = '\0';
-	return (dest_size);
+	return (i);
 }

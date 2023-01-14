df0a228908e359958f79d56524cb9e1abf3e2877
anna-balan <dp101292baa@gmail.com>
2023-01-14 09:18:06 +0100

    fixed strncmp issue

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,11 +17,16 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	size_t	buff;
 
 	i = 0;
 	while ((s1[i] || s2[i]) && (i < n))
-	{
-		return (((unsigned char)s1[i] - (unsigned char)s2[i]));
+	{	
+		buff = (unsigned char)s1[i] - (unsigned char)s2[i];
+		if (buff != 0)
+		{
+			return (buff);
+		}
 		i++;
 	}
 	return (0);
@@ -30,7 +35,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 // int	main(void)
 // {
 // 	char str[100] = "xearningisfun";
-// 	char str2[100] = "test";
-// 	printf("%d", ft_strncmp(str, str2, 0));
+// 	char str2[100] = "xast";
+// 	printf("%d", ft_strncmp(str, str2, 2));
 // 	return (0);
 // }

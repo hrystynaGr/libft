75cfbbbf7c00133135e87aa891d35a3fdd5e117e
anna-balan <dp101292baa@gmail.com>
2023-01-31 14:09:22 +0100

    fixes for trim function, Makefile and Libft file changes for bonus part

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,80 +12,23 @@
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set);
-
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (*str != '\0')
-	{
-		str++;
-		i++;
-	}
-	return (i);
-}
-
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
-{
-	size_t	i;
-
-	i = 0;
-	if (size != 0)
-	{
-		size -= 1;
-		while (src[i] && (i < size))
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		dest[i] = '\0';
-	}
-	return (i);
-}
-
-char	*ft_front(char const *set, int j, char *res)
-{
-	while (set[++j] != '\0')
-	{
-		if (*res == set[j])
-		{
-			j = -1;
-			res++;
-		}
-	}
-	return (res);
-}
-
-char	*ft_back(char const *set, int j, char *res, int len)
-{
-	while (set[++j] != '\0')
-	{
-		if (res[len] == set[j])
-		{
-			j = -1;
-			res[len--] = '\0';
-		}
-	}
-	return (res);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		j;
-	char	*res;
+	size_t	len;
 
-	res = (char *)malloc(sizeof(char) * ft_strlen(s1));
-	ft_strlcpy(res, s1, ft_strlen(s1) + 1);
-	res = ft_front(set, j = 0, res);
-	ft_back(set, 0, res, ft_strlen(res) - 1);
-	return (res);
+	if (!s1 || !set)
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (len && ft_strrchr(set, s1[len]))
+		len--;
+	return (ft_substr(s1, 0, len + 1));
 }
 
-// int	main(void)
-// {
-// 	const char		s1[30] = "unl8ock";
-// 	const char		set[30] = "klockun";
-// 	puts(ft_strtrim(s1, set));
-// }
+/*int	main(void)
+{
+	const char		s1[30] = "aunl8a";
+	const char		set[30] = "klckun";
+	puts(ft_strtrim(s1, set));
+}*/
--- libft.h
+++ libft.h
@@ -16,6 +16,23 @@
 # include <stdlib.h>
 # include <stddef.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}			t_list;
+
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstnew(void *content);
+t_list				*ft_lstlast(t_list *lst);
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
+void				ft_lstadd_front(t_list **lst, t_list *new);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+
 void				ft_bzero(void *s, size_t n);
 void				ft_striteri(char *s, void (*f)(unsigned int, char*));
 void				ft_putchar_fd(char c, int fd);
